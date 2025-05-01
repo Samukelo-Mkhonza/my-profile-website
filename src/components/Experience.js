492213e3a0cb6314f1fcaaa73cb18eb33c835887
Samukelo Mkhonza <Mkhonzasenzo525@gmail.com>
2025-05-01 13:20:57 +0200

    refactor: improve layout and alignment in Experience component

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -6,10 +6,12 @@ const Section = styled.section`
   padding: 5rem 2rem;
   background: #f7f7f7;
 `;
+
 const Container = styled.div`
   max-width: 720px;
   margin: 0 auto;
 `;
+
 const Heading = styled.h2`
   font-size: 2.5rem;
   font-weight: 600;
@@ -18,6 +20,7 @@ const Heading = styled.h2`
   text-align: center;
   margin-bottom: 2rem;
 `;
+
 const Timeline = styled.div`
   position: relative;
   &:before {
@@ -31,6 +34,7 @@ const Timeline = styled.div`
     transform: translateX(-50%);
   }
 `;
+
 const Entry = styled(motion.div)`
   position: relative;
   width: 50%;
@@ -38,17 +42,23 @@ const Entry = styled(motion.div)`
   background: #ffffff;
   border-radius: 4px;
   margin-bottom: 2rem;
-  ${props => (props.align === 'left' ? 'left: 0; text-align: right;' : 'left: 50%; text-align: left;')}
+  ${props => (props.align === 'left' 
+    ? 'left: 0; text-align: right; padding-right: 3rem;' 
+    : 'left: 50%; text-align: left; padding-left: 3rem;'
+  )}
 `;
+
 const Circle = styled.div`
   position: absolute;
   top: 1rem;
-  left: 50%;
   width: 12px;
   height: 12px;
   background: #000;
   border-radius: 50%;
-  transform: translateX(-50%);
+  ${props => props.align === 'left' 
+    ? 'right: -6px;' 
+    : 'left: -6px;'
+  }
 `;
 
 const events = [
@@ -61,23 +71,27 @@ const Experience = () => (
     <Container>
       <Heading>Experience</Heading>
       <Timeline>
-        {events.map((e, i) => (
-          <Entry
-            key={i}
-            align={i % 2 === 0 ? 'left' : 'right'}
-            initial={{ opacity: 0, x: i % 2 === 0 ? -100 : 100 }}
-            whileInView={{ opacity: 1, x: 0 }}
-            transition={{ duration: 0.5 }}
-            viewport={{ once: true }}
-          >
-            <Circle />
-            <h3 style={{ margin: '0.5rem 0' }}>{e.period}</h3>
-            <h4 style={{ margin: '0.25rem 0', fontWeight: 500 }}>{e.role}</h4>
-            <p style={{ marginTop: '0.5rem', color: '#333' }}>{e.desc}</p>
-          </Entry>
-        ))}
+        {events.map((e, i) => {
+          const alignment = i % 2 === 0 ? 'left' : 'right';
+          return (
+            <Entry
+              key={i}
+              align={alignment}
+              initial={{ opacity: 0, x: alignment === 'left' ? -100 : 100 }}
+              whileInView={{ opacity: 1, x: 0 }}
+              transition={{ duration: 0.5 }}
+              viewport={{ once: true }}
+            >
+              <Circle align={alignment} />
+              <h3 style={{ margin: '0.5rem 0' }}>{e.period}</h3>
+              <h4 style={{ margin: '0.25rem 0', fontWeight: 500 }}>{e.role}</h4>
+              <p style={{ marginTop: '0.5rem', color: '#333' }}>{e.desc}</p>
+            </Entry>
+          );
+        })}
       </Timeline>
     </Container>
   </Section>
 );
+
 export default Experience;
